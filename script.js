e197c689af41f84357f73c30cca669941c083530
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2021-12-10 16:47:17 -0300

    Requisito 6 ok

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function meuBotaoLimpaCores(){
 
+
+function createPalette() {
+  const selectPalette = document.getElementById("color-palette");
+  console.log(selectPalette);
+  for (let i = 0; i < 4; i += 1) {
+    let createDiv = document.createElement("div");
+    createDiv.className = "color";
+    selectPalette.appendChild(createDiv)
+  }
 }
+createPalette()
+
+const divFilho = document.getElementsByClassName("color")[0];
+console.log(divFilho);
+divFilho.className = "selected color";
