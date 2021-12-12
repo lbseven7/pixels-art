1d63d7519fffe91dbf58ae706db1de929112f6e4
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2021-12-12 19:47:41 -0300

    Requisitos 9, 10  parcial

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function createPalette() {
   const selectPalette = document.getElementById("color-palette");
-  console.log(selectPalette);
+
   for (let i = 0; i < 4; i += 1) {
     let createDiv = document.createElement("div");
     createDiv.className = "color";
@@ -15,4 +15,15 @@ const divFilho = document.getElementsByClassName("color")[0];
 console.log(divFilho);
 divFilho.className = "selected color";
 
+/*function selectColor(event) {
+  const colors = document.querySelectorAll('.color');
+  for (let i = 0; i < colors.length; i += 1) {
+    console.log(colors[i]);
+    colors[i].classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+Selection.addEventListener('click', selectColor);*/
+function pegaCor() {
 
+}
