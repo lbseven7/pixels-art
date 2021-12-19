917c6dbf4fd7949e053331d589c5b210282b0373
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2021-12-19 15:13:41 -0300

    requisito 7, 8, 9

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
+let currentSelectColor = 'black'
 
+function handleColor(event) {
+  let selectedDiv = document.querySelector('.selected')
+  let currentDiv = event.target
 
-function createPalette() {
+  selectedDiv.classList.remove('selected')
+  currentDiv.classList.add('selected')
+  currentSelectColor = window.getComputedStyle(currentDiv, null).getPropertyValue('background-color');
+
+}
+
+function createPalette(colors) {
   const selectPalette = document.getElementById("color-palette");
 
-  for (let i = 0; i < 4; i += 1) {
+  for (let i = 0; i < colors.length; i += 1) {
     let createDiv = document.createElement("div");
+    createDiv.style.backgroundColor = colors[i]
     createDiv.className = "color";
+    if (colors[i] === 'black') {
+      createDiv.classList.add('selected')
+    }
+    createDiv.addEventListener('click', handleColor)
+
     selectPalette.appendChild(createDiv)
   }
 }
-createPalette()
 
-const divFilho = document.getElementsByClassName("color")[0];
-console.log(divFilho);
-divFilho.className = "selected color";
+function creatBoard(size) {
 
-/*function selectColor(event) {
-  const colors = document.querySelectorAll('.color');
-  for (let i = 0; i < colors.length; i += 1) {
-    console.log(colors[i]);
-    colors[i].classList.remove('selected');
-    event.target.classList.add('selected');
+  for (let index = 0; index < size; index += 1) {
+    let boardPixels = document.getElementById("pixel-board");
+    let createPixels = document.createElement("div");
+    let rowClass = `row-board${index}`
+    createPixels.className = rowClass;
+    boardPixels.appendChild(createPixels)
+
+    for (index2 = 0; index2 < size; index2 += 1) {
+      let rowBoard = document.getElementsByClassName(rowClass)[0];
+      let pixelDiv = document.createElement('div')
+      pixelDiv.className = 'pixel'
+      rowBoard.appendChild(pixelDiv)
+    }
+    boardPixels.appendChild(createPixels)
   }
 }
-Selection.addEventListener('click', selectColor);*/
-function pegaCor() {
 
+window.onload = function () {
+  createPalette(['black', 'red', 'blue', 'green'])
+
+  creatBoard(5)
 }
+
+
